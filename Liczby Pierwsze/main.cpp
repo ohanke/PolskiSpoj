0a955cdd3f87473f258a82e75d0c03e81d87a61e
ohanke <47105713+ohanke@users.noreply.github.com>
2019-01-30 19:34:58 +0100

    Update main.cpp

--- Liczby Pierwsze/main.cpp
+++ Liczby Pierwsze/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     int n,x;
+    cout << "Podaj liczbe. Sprawdze czy jest ona pierwsza: ";
     cin >> n;
     if(n<2) x=1;
     for(int i=2;i*i<=n;i++)
@@ -12,7 +13,7 @@ int main()
         if(n%i==0) x=1;
         else x=0;
     }
-    if(x==0)cout<<"TAK";
-    else cout<<"NIE";
+    if(x==0)cout<<"Jest to liczba pierwsza.";
+    else cout<<"To nie jest liczba pierwsza";
     return 0;
 }
