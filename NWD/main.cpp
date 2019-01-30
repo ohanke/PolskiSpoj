4f9ef5c3f9adf3682bb89c372296b6bcfc266ed1
ohanke <47105713+ohanke@users.noreply.github.com>
2019-01-30 19:45:31 +0100

    Add files via upload

---
+++ NWD/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    while((a*b)!=0)
+        {
+        if(a>b)a%=b;
+        else if(b>a)b%=a;
+        else if(a==b)return 1;
+        }
+    if(a>b) cout<<a;
+    else cout<<b;
+    return 0;
+}
