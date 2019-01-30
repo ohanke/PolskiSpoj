5d7a27fcba152114bd487b7a6574e0897c7b1b3a
ohanke <47105713+ohanke@users.noreply.github.com>
2019-01-30 19:46:40 +0100

    Update main.cpp

--- NWD/main.cpp
+++ NWD/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     int a,b;
+    cout<<"Podaj liczbe a i b, z ktorych chcesz obliczyc NWD: ";
     cin>>a>>b;
     while((a*b)!=0)
         {
@@ -12,7 +13,7 @@ int main()
         else if(b>a)b%=a;
         else if(a==b)return 1;
         }
-    if(a>b) cout<<a;
+    if(a>b) cout<<"NWD tych liczb to: "<<a;
     else cout<<b;
     return 0;
 }
