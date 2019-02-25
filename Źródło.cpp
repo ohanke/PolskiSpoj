326a4334dad056247237016c6689e219fdb55146
ohanke <47105713+ohanke@users.noreply.github.com>
2019-02-25 22:18:21 +0100

    Add files via upload

---
+++ Źródło.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,s=1;
+	cin >> x;
+
+	for (int i = 1; i <= x; i++)
+	{
+		s *= i;
+	}
+	
+	int a, b;
+	if (s < 10) { a = 0; b = s; }
+	else if (s >= 10) { a = s / 10; b = s % (a * 10); }
+
+	cout << a << " " << b;
+	system("pause");
+
+	return 0;
+}
