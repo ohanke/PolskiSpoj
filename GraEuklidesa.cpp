3b917269b6c077de8ea42fe8c6a426f13b426535
ohanke <47105713+ohanke@users.noreply.github.com>
2020-05-18 19:18:59 +0200

    Update GraEuklidesa.cpp

--- GraEuklidesa.cpp
+++ GraEuklidesa.cpp
@@ -4,18 +4,24 @@ using namespace std;
 
 int main()
 {
-	int a, b, c;
-	cin >> a >> b;
-	
-	do
-	{
-		if (a > b) a -= b;
-		else if (a < b) b -= a;
-	} while (a != b);
-	
-	c = a + b;
-	cout << c;
-	
-	system("pause");
-	return 0;
+    int a, b, c, n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+
+        while (a != b)
+        {
+            if (a > b)
+                a -= b;
+            if (a < b)
+                b -= a;
+        }
+
+        c = a + b;
+        cout << c << endl;
+    }
+    
+    return 0;
 }
